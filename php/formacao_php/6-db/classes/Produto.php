73407faa0472b76fd93430a1cba750b11b4b7158
Bruno Cesar <bruunoocesar@gmail.com>
2020-02-14 21:29:27 -0300

    formacao_php#6 - Adiciona index de produtos

---
+++ php/formacao_php/6-db/classes/Produto.php
@@ -0,0 +1,24 @@
+<?php
+
+class Produto
+{
+    public $id;
+    public $nome;
+    public $preco;
+    public $quantidade;
+    public $categoria_id;
+
+    public static function listar()
+    {
+        $query =   "SELECT p.id, p.nome, p.preco, p.quantidade, p.categoria_id, c.nome as categoria_nome 
+                    FROM produtos p
+                    INNER JOIN categorias c ON p.categoria_id = c.id
+                    ORDER BY p.nome";
+        
+        $conexao = Conexao::pegarConexao();
+        $resultado = $conexao->query($query);
+        $lista = $resultado->fetchAll();
+
+        return $lista;
+    }
+}
