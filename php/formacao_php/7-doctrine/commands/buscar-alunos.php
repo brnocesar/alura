9356917efb31374207761cdc25ec50cafea9a8c5
Bruno Cesar <bruunoocesar@gmail.com>
2020-03-30 19:44:45 -0300

    formacao_php#7 - Implementa consulta para recuperar registros

---
+++ php/formacao_php/7-doctrine/commands/buscar-alunos.php
@@ -0,0 +1,52 @@
+<?php
+
+use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Helper\EntityManagerFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+
+$repositorioDeAlunos = $entityManager->getRepository(Aluno::class);
+
+
+if ( isset($argv[1]) ) {
+
+    if ( is_numeric($argv[1]) ) {
+
+        if ( $alunoEspecifico = $repositorioDeAlunos->find($argv[1]) ) {
+            echo "\n{$alunoEspecifico->getId()}. {$alunoEspecifico->getNome()}\n";
+            return;
+        }
+        else {
+            echo "\nAluno não encontrado pelo ID\n";
+        }
+    }
+    else {
+
+        $alunosEspecificos = $repositorioDeAlunos->findBy([
+            'nome' => $argv[1]
+        ]);
+
+        if ( $alunosEspecificos ) {
+
+            foreach ($alunosEspecificos as $aluno) {
+                echo "{$aluno->getId()}. {$aluno->getNome()}\n";
+            }
+            return;
+        }
+        else {
+            echo "\nNenhum aluno encontrado pelo Nome\n";
+        }
+    }    
+}
+
+/**
+ * @var Aluno[] $listaDeAlunos
+*/
+$listaDeAlunos = $repositorioDeAlunos->findAll();
+
+foreach ($listaDeAlunos as $aluno) {
+    echo "{$aluno->getId()}. {$aluno->getNome()}\n";
+}
--- php/formacao_php/7-doctrine/commands/criar-aluno.php
+++ php/formacao_php/7-doctrine/commands/criar-aluno.php
@@ -6,7 +6,7 @@ use Alura\Doctrine\Helper\EntityManagerFactory;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $aluno = new Aluno();
-$aluno->setNome('Bruno Cesar 2');
+$aluno->setNome($argv[1]);
 
 $entityManagerFactory = new EntityManagerFactory();
 $entityManager = $entityManagerFactory->getEntityManager();
