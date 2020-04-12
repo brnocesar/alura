c61b8c38406e867321d681ab5b5662f1a78d45dc
Bruno Cesar <bruunoocesar@gmail.com>
2020-04-12 11:56:54 -0300

    formacao_php#8 - Adiciona webservice que retorna cursos em xml

--- php/formacao_php/8-mvc/config/routes.php
+++ php/formacao_php/8-mvc/config/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use Alura\Cursos\Controller\CursosEmJson;
+use Alura\Cursos\Controller\CursosEmXml;
 use Alura\Cursos\Controller\Deslogar;
 use Alura\Cursos\Controller\Exclusao;
 use Alura\Cursos\Controller\FormularioEdicao;
@@ -20,4 +21,5 @@ return [
     '/realizar-login'   => RealizarLogin::class,
     '/logout'           => Deslogar::class,
     '/buscarCursosEmJson' => CursosEmJson::class,
+    '/buscarCursosEmXml' => CursosEmXml::class,
 ];
