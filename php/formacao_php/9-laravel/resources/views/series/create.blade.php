150d0e6bc851cd8046bcf8f324daef01656ff09d
Bruno Cesar <bruunoocesar@gmail.com>
2020-04-20 19:41:43 -0300

    formacao_php#9 - Modifica formulario para receber info sobre temporadas e episodios

--- php/formacao_php/9-laravel/resources/views/series/create.blade.php
+++ php/formacao_php/9-laravel/resources/views/series/create.blade.php
@@ -22,11 +22,21 @@ Adicionar Série
 
 <form method="post" action="{{ route('registra_serie') }}">
     @csrf
-    <div class="form-group">
-        <label for="nome">Nome</label>
-        <input id="nome" type="text" class="form-control" name="nome">
+    <div class="row">
+        <div class="col col-8">
+            <label for="nome">Nome</label>
+            <input id="nome" type="text" class="form-control" name="nome">
+        </div>
+        <div class="col col-2">
+            <label for="qtd_temporadas">Temporadas</label>
+            <input type="number" class="form-control" id="qtd_temporadas" name="qtd_temporadas">
+        </div>
+        <div class="col col-2">
+            <label for="ep_por_temporada">Ep. por temporada</label>
+            <input type="number" class="form-control" id="ep_por_temporada" name="ep_por_temporada">
+        </div>
     </div>
-    <div class="d-flex justify-content-end">
+    <div class="d-flex justify-content-end mt-2">
         <button class="btn btn-primary"><i class="fas fa-save"></i></button>
     </div>
 </form>
