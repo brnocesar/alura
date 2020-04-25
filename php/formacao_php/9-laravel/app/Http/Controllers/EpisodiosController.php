c5438a7c00edef627d581a4fe81f5a64dd5b1631
Bruno Cesar <bruunoocesar@gmail.com>
2020-04-25 15:27:54 -0300

    formacao_php#9 - Injeta dependencia da classe Temporada no index() de EpisodiosController

--- php/formacao_php/9-laravel/app/Http/Controllers/EpisodiosController.php
+++ php/formacao_php/9-laravel/app/Http/Controllers/EpisodiosController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Temporada;
 use Illuminate\Http\Request;
 
 class EpisodiosController extends Controller
 {
-    public function index(int $temporadaId)
+    public function index(Temporada $temporada)
     {
-
+        dd($temporada);
     }
 }
--- php/formacao_php/9-laravel/routes/web.php
+++ php/formacao_php/9-laravel/routes/web.php
@@ -18,4 +18,4 @@ Route::delete('/series/{id}', 'SeriesController@destroy')->name('deleta_serie');
 Route::post('/series/{id}/editar-nome', 'SeriesController@editarNome');
 
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index')->name('listar_temporadas');
-Route::get('temporadas/{temporadaId}/episodios', 'EpisodiosController@index')->name('listar_episodios');
+Route::get('temporadas/{temporada}/episodios', 'EpisodiosController@index')->name('listar_episodios');
