4a9ed10e565a3a21d75e4980124fb226ed8e1f4e
Bruno Cesar <bruunoocesar@gmail.com>
2020-04-24 23:00:48 -0300

    formacao_php#9 - Refatora metodo de criar serie separando as responsabilidades e usando 'transaction'

--- php/formacao_php/9-laravel/app/Service/CriadorDeSerie.php
+++ php/formacao_php/9-laravel/app/Service/CriadorDeSerie.php
@@ -3,21 +3,35 @@
 namespace App\Service;
 
 use App\Serie;
+use App\Temporada;
+use Illuminate\Support\Facades\DB;
 
 class CriadorDeSerie
 {
     public function criarSerie(string $nomeSerie, int $qtdTemporadas, int $epPorTemporada): Serie
     {
-        $serie = Serie::create(['nome' => $nomeSerie]);
+        $serie = '';
+        DB::transaction(function () use(&$serie, $nomeSerie, $qtdTemporadas, $epPorTemporada) {
 
-        for ($i = 1; $i <= $qtdTemporadas; $i++) {
-            $temporada = $serie->temporadas()->create(['numero' => $i]);
-
-            for ($j=1; $j <= $epPorTemporada; $j++) {
-                $temporada->episodios()->create(['numero' => $j]);
-            }
-        }
+            $serie = Serie::create(['nome' => $nomeSerie]);
+            $this->criarTemporadas($serie, $qtdTemporadas, $epPorTemporada);
+        });
 
         return $serie;
+    }
+
+    private function criarTemporadas(Serie $serie, int $qtdTemporadas, int $epPorTemporada): void
+    {
+        for ($i = 1; $i <= $qtdTemporadas; $i++) {
+            $temporada = $serie->temporadas()->create(['numero' => $i]);
+            $this->criarEpisodios($temporada, $epPorTemporada);
+        }
+    }
+
+    private function criarEpisodios(Temporada $temporada, int $epPorTemporada): void
+    {
+        for ($j=1; $j <= $epPorTemporada; $j++) {
+            $temporada->episodios()->create(['numero' => $j]);
+        }
     }
 }
