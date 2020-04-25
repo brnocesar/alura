ac038ecb64b2d112c4d9f42d74221931d7885dc8
Bruno Cesar <bruunoocesar@gmail.com>
2020-04-25 17:25:11 -0300

    formacao_php#9 - Envia episodios assistidos como vetor

--- php/formacao_php/9-laravel/resources/views/episodios/index.blade.php
+++ php/formacao_php/9-laravel/resources/views/episodios/index.blade.php
@@ -23,7 +23,7 @@ Episódios da {{$temporada->numero}}<sup>a</sup> Temporada de {{$temporada->seri
         @foreach ($temporada->episodios as $episodio)
             <li class="list-group-item d-flex justify-content-between align-items-center">
                 Episódio {{ $episodio->numero }}
-                <input type="checkbox">
+                <input type="checkbox" name="episodios[]" value="{{ $episodio->id }}">
             </li>
         @endforeach
     </ul>
