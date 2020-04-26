8651f5db3da115d202bb0120cabedfa72deed80a
Bruno Cesar <bruunoocesar@gmail.com>
2020-04-26 17:23:38 -0300

    formacao_php#9 - Desfaz alteracao no middleware padrao de autenticacao e adiciona ajustes nas views de login

--- formacao_alura/9-laravel/resources/views/autenticacao/create.blade.php
+++ formacao_alura/9-laravel/resources/views/autenticacao/create.blade.php
@@ -8,29 +8,33 @@ Criar usuário
 
 @include('mensagem')
 
-<form method="POST">
-    @csrf
+<div class="container col-6 justify-content-center">
 
-    <div class="form-group">
-        <label for="name">Nome</label>
-        <input type="text" name="name" id="name" required class="form-control">
-    </div>
+    <form method="POST">
+        @csrf
 
-    <div class="form-group">
-        <label for="email">E-mail</label>
-        <input type="email" name="email" id="email" required class="form-control">
-    </div>
+        <div class="form-group">
+            <label for="name">Nome</label>
+            <input type="text" name="name" id="name" required class="form-control">
+        </div>
 
-    <div class="form-group">
-        <label for="password">Senha</label>
-        <input type="password" name="password" id="password" required min="1" class="form-control">
-    </div>
+        <div class="form-group">
+            <label for="email">E-mail</label>
+            <input type="email" name="email" id="email" required class="form-control">
+        </div>
 
-    <div class="d-flex justify-content-end">
-        <button type="submit" class="btn btn-primary mb-2 mr-2" style="font-size: 80%">
-            <i class="fas fa-user-plus mr-2"></i>Criar usuário
-        </button>
-    </div>
-</form>
+        <div class="form-group">
+            <label for="password">Senha</label>
+            <input type="password" name="password" id="password" required min="1" class="form-control">
+        </div>
+
+        <div class="d-flex justify-content-center">
+            <button type="submit" class="btn btn-primary mb-2 mr-2" style="font-size: 80%">
+                <i class="fas fa-user-plus mr-2"></i>Criar usuário
+            </button>
+        </div>
+    </form>
+
+</div>
 
 @endsection
--- formacao_alura/9-laravel/resources/views/autenticacao/index.blade.php
+++ formacao_alura/9-laravel/resources/views/autenticacao/index.blade.php
@@ -8,30 +8,34 @@ Entrar
 
 @include('mensagem')
 
-<form method="POST">
-    @csrf
+<div class="container col-6 justify-content-center">
 
-    <div class="form-group">
-        <label for="email">E-mail</label>
-        <input type="email" name="email" id="email" required class="form-control">
-    </div>
+    <form method="POST">
+        @csrf
 
-    <div class="form-group">
-        <label for="password">Senha</label>
-        <input type="password" name="password" id="password" required min="1" class="form-control">
-    </div>
+        <div class="form-group">
+            <label for="email">E-mail</label>
+            <input type="email" name="email" id="email" required class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="password">Senha</label>
+            <input type="password" name="password" id="password" required min="1" class="form-control">
+        </div>
+
+        <div class="d-flex justify-content-center">
+            <button type="submit" class="btn btn-primary mb-2 mt-2" style="font-size: 80%">
+                <i class="fas fa-sign-in-alt mr-2"></i>Entrar
+            </button>
+        </div>
+    </form>
 
     <div class="d-flex justify-content-center">
-        <button type="submit" class="btn btn-primary mb-2 mr-2" style="font-size: 80%">
-            <i class="fas fa-sign-in-alt mr-2"></i>Entrar
-        </button>
+        <a class="btn btn-dark mb-2 mt-2" style="font-size: 80%" href="{{ route('pagina_registro') }}">
+            <i class="fas fa-user-plus mr-2"></i>Criar usuário
+        </a>
     </div>
-</form>
 
-<div class="d-flex justify-content-center">
-    <a class="btn btn-dark mb-2 mr-2" style="font-size: 80%" href="{{ route('pagina_registro') }}">
-        <i class="fas fa-user-plus mr-2"></i>Criar usuário
-    </a>
 </div>
 
 @endsection
