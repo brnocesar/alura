06a3c8c4ca6c47b7ba2084a782f8443045bc3674
Bruno Cesar <bruunoocesar@gmail.com>
2020-04-26 18:03:24 -0300

    formacao_php#9 - Adiciona verificacao no middleware proprio

--- formacao_alura/9-laravel/app/Http/Middleware/Autenticador.php
+++ formacao_alura/9-laravel/app/Http/Middleware/Autenticador.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class Autenticador
 {
@@ -15,6 +16,10 @@ class Autenticador
      */
     public function handle($request, Closure $next)
     {
+        if ( !Auth::check() ) {
+            return redirect()->route('pagina_login');
+        }
+
         return $next($request);
     }
 }
