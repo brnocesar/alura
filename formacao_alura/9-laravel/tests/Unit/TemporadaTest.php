40b62a369b92e9580247e7d9d5c3a6ad3a8c05c6
Bruno Cesar <bruunoocesar@gmail.com>
2020-04-27 21:01:47 -0300

    formacao_php#9 - Escreve teste para quantidade de episodios assistidos

---
+++ formacao_alura/9-laravel/tests/Unit/TemporadaTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Episodio;
+use App\Temporada;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TemporadaTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $temporada = new Temporada();
+
+        $episodio1 = new Episodio();
+        $episodio1->assistido = true;
+        $episodio2 = new Episodio();
+        $episodio2->assistido = false;
+        $episodio3 = new Episodio();
+        $episodio3->assistido = true;
+
+        $temporada->episodios->add($episodio1);
+        $temporada->episodios->add($episodio2);
+        $temporada->episodios->add($episodio3);
+
+        $episodiosAssistidos = $temporada->getEpisodiosAssistidos();
+        $this->assertCount(2, $episodiosAssistidos);
+    }
+}
