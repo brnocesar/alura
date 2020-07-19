f4db4e01ef98fc4448a084894bbd4f4e5d85de7d
Bruno Cesar <bruunoocesar@gmail.com>
2020-07-19 17:27:19 -0300

    f#9 - Cria template para o email

--- 9-laravel/routes/web.php
+++ 9-laravel/routes/web.php
@@ -33,3 +33,7 @@ Route::post('/entrar', 'AutenticacaoController@entrar')->name('realizar_login');
 Route::get('/registrar', 'AutenticacaoController@create')->name('pagina_registro');
 Route::post('/registrar', 'AutenticacaoController@store')->name('realizar_registro');
 Route::get('/sair', 'AutenticacaoController@sair')->name('deslogar');
+
+Route::get('/mail-teste', function (){
+    return new App\Mail\NovaSerie();
+});
