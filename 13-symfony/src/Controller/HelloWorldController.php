560635fd6b64d2ec28f5076f189dbf2a24c74508
Bruno Cesar <bruunoocesar@gmail.com>
2020-08-12 00:18:55 -0300

    #13 - Hello world

---
+++ 13-symfony/src/Controller/HelloWorldController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloWorldController
+{
+    /**
+     * @Route("/hello")
+    */
+    public function helloWorldAction(Request $request): Response
+    {
+        return new JsonResponse([
+            "message"     => "Hello world",
+            "pathInfo"    => $request->getPathInfo(),
+            "parameter"   => $request->query->get('parameter'),
+            "queryParams" => $request->query->all(),
+        ]);
+    }
+}
