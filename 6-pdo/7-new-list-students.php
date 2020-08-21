0281856c1bdf0e35d748de1f7befd8bc9cb7929d
Bruno Cesar <bruunoocesar@gmail.com>
2020-08-21 18:27:56 -0300

    #6 - Realiza consulta por alunos e telefones com JOIN

--- 6-pdo/7-new-list-students.php
+++ 6-pdo/7-new-list-students.php
@@ -7,6 +7,10 @@ require_once 'vendor/autoload.php';
 
 $pdo = ConnectionCreator::createConnection();
 $repository = new PdoStudentRepository($pdo);
-$studentList = $repository->allStudents();
+
+// $studentList = $repository->allStudents();
+$studentList = $repository->studentsWithPhone();
 
 var_dump($studentList);
+
+echo "\n{$studentList[1]->phones()[0]->formattedPhone()}\n";
--- 6-pdo/src/Domain/Repository/StudentRepository.php
+++ 6-pdo/src/Domain/Repository/StudentRepository.php
@@ -10,11 +10,10 @@ use PDOStatement;
 interface StudentRepository
 {
     public function allStudents(): array;
+    public function studentsWithPhone(): array;
     public function studentsBirthAt(DateTimeInterface $birthDate): array;
     // public function hydrateStudentList(PDO $stmt): array;
     public function hydrateStudentList(PDOStatement $stmt): array;
     public function save(Student $student): bool;
-    public function insert(Student $student): bool;
-    public function update(Student $student): bool;
     public function remove(Student $student): bool;
 }
