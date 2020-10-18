2f1a84e792298bd31138a1933d43343e95fd44c7
Bruno Cesar <bruunoocesar@gmail.com>
2020-10-18 08:56:51 -0300

    #13 - Renomeia controller de especialidades

--- 13-symfony/src/Controller/EspecialidadeController.php
+++ 13-symfony/src/Controller/EspecialidadeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class EspecialidadesController extends AbstractController
+class EspecialidadeController extends AbstractController
 {
     private $entityManager;
     private $repository;
