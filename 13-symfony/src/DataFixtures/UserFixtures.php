4a2c8be27c7874a71184b9fc9f991d9e5d4bc030
Bruno Cesar <bruunoocesar@gmail.com>
2020-10-31 17:19:05 -0300

    #13 - Cria usuário de para testes através da fixture

--- 13-symfony/src/DataFixtures/UserFixtures.php
+++ 13-symfony/src/DataFixtures/UserFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,11 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setUsername('usuario')
+            ->setPassword('$argon2id$v=19$m=65536,t=4,p=1$6JiRycZUeQ93a/7IOJzexA$EiUmr0GDBrE8uUvvK5vvPj8BTv4vxBaTeQH5H/xxFbg'); // 123456
+        
+        $manager->persist($user);
 
         $manager->flush();
     }
