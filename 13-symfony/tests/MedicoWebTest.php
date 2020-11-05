ec676671f8586a65a2ab2a896fdfbf7257a6d684
Bruno Cesar <bruunoocesar@gmail.com>
2020-11-04 21:39:32 -0300

    #13 - Implementa teste com autenticação

--- 13-symfony/tests/MedicoWebTest.php
+++ 13-symfony/tests/MedicoWebTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class MedicoWebTest extends WebTestCase
@@ -12,5 +13,39 @@ class MedicoWebTest extends WebTestCase
         $client->request('GET', '/medicos');
 
         $this->assertEquals(401, $client->getResponse()->getStatusCode());
+    }
+
+    public function testEspecialidadesSaoListadasAoAutenticar()
+    {
+        $client = static::createClient();
+        $token = $this->login($client);
+        $client->request('GET', '/medicos', [], [], [
+            'HTTP_AUTHORIZATION' => "Bearer $token"
+        ]);
+
+        $body = json_decode($client->getResponse()->getContent());
+        $currentPage = $body->paginaAtual ?? null;
+
+        $this->assertEquals(1, $currentPage);
+    }
+
+    private function login(KernelBrowser $client): string
+    {
+        $client->request(
+            'POST', '/login', 
+            [], 
+            [], 
+            [
+                'CONTENT_TYPE' => 'application/json'
+            ],
+            json_encode([
+                'username' => 'usuario',
+                'password' => '123456'
+            ])
+        );
+
+        $token = json_decode($client->getResponse()->getContent());
+        
+        return $token->access_token;
     }
 }
